e83024c647a745e176cdc8ad61a53731fc84caa0
GmtechSolution <gmtech19890@gmail.com>
2023-03-26 15:53:52 +0800

    update

---
+++ thankyou.js
@@ -0,0 +1,25 @@
+const stars = document.querySelectorAll('.star');
+const message = document.querySelector('.message');
+const submit = document.querySelector('.submit');
+
+let rating = 0;
+
+stars.forEach(star => {
+  star.addEventListener('click', () => {
+    rating = star.getAttribute('data-star');
+    message.innerText = `You rated this product ${rating} stars`;
+    stars.forEach(s => s.classList.remove('selected'));
+    star.classList.add('selected');
+  });
+});
+
+submit.addEventListener('click', () => {
+  if (rating > 0) {
+    alert(`Thank you for rating this product ${rating} stars`);
+    rating = 0;
+    message.innerText = '';
+    stars.forEach(s => s.classList.remove('selected'));
+  } else {
+    alert('Please select a rating');
+  }
+});
