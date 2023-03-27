ec8c4eae031526bd94a4b1fdfb1c6626da4b0bfc
GmtechSolution <gmtech19890@gmail.com>
2023-03-28 02:17:35 +0800

    update

--- script.js
+++ script.js
@@ -10,11 +10,17 @@ function searchGames() {
     for (i = 0; i < li.length; i++) {
       a = li[i].getElementsByTagName("h4")[0];
       txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      if (txtValue.toUpperCase().OfflinegamesOf(filter) > -1) { 
         li[i].style.display = "";
       } else {
         li[i].style.display = "none";
       }
     }
   }
+
+//   To use this code, simply copy and paste the HTML, CSS, and JS code into separate files named
+//    index.html, style.css, and script.js, respectively. Then, open the index.html file in a
+//     web browser to see the search bar in action.
+
+// if (txtValue.toUpperCase().indexOf(filter) > -1) {
   
