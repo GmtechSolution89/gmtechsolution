00ad9e239d5f68dc41e8399a483fa4e5dcaa5244
GmtechSolution <gmtech19890@gmail.com>
2023-03-28 02:29:37 +0800

    update

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-function searchGames() {
-    // Declare variables
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('search-bar');
-    filter = input.value.toUpperCase();
-    ul = document.getElementsByClassName("game-list")[0];
-    li = ul.getElementsByTagName('h4');
+// function searchGames() {
+//     // Declare variables
+//     var input, filter, ul, li, a, i, txtValue;
+//     input = document.getElementById('search-bar');
+//     filter = input.value.toUpperCase();
+//     ul = document.getElementsByClassName("game-list")[0];
+//     li = ul.getElementsByTagName('h4');
   
-    // Loop through all games and hide those that don't match the search query
-    for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("h4")[0];
-      txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().OfflinegamesOf(filter) > -1) { 
-        li[i].style.display = "";
-      } else {
-        li[i].style.display = "none";
-      }
-    }
-  }
+//     // Loop through all games and hide those that don't match the search query
+//     for (i = 0; i < li.length; i++) {
+//       a = li[i].getElementsByTagName("h4")[0];
+//       txtValue = a.textContent || a.innerText;
+//       if (txtValue.toUpperCase().indexOf(filter) > -1) { 
+//         li[i].style.display = "";
+//       } else {
+//         li[i].style.display = "none";
+//       }
+//     }
+//   }
 
 //   To use this code, simply copy and paste the HTML, CSS, and JS code into separate files named
 //    index.html, style.css, and script.js, respectively. Then, open the index.html file in a
